b0a4dfe2bb6e2191a81193f38cb7d7be6a3a51c8
alexandrustf <37770439+alexandrustf@users.noreply.github.com>
2020-04-12 20:21:31 +0300

    Added Interface

--- MyPhotos.GUI.WCF/proxy.cs
+++ MyPhotos.GUI.WCF/proxy.cs
@@ -11,36 +11,36 @@
 namespace MyPhotos.Persistence
 {
     using System.Runtime.Serialization;
-    
-    
+
+
     [System.Diagnostics.DebuggerStepThroughAttribute()]
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Runtime.Serialization", "4.0.0.0")]
-    [System.Runtime.Serialization.DataContractAttribute(Name="Photo", Namespace="http://schemas.datacontract.org/2004/07/MyPhotos.Persistence")]
+    [System.Runtime.Serialization.DataContractAttribute(Name = "Photo", Namespace = "http://schemas.datacontract.org/2004/07/MyPhotos.Persistence")]
     public partial class Photo : object, System.Runtime.Serialization.IExtensibleDataObject
     {
-        
+
         private System.Runtime.Serialization.ExtensionDataObject extensionDataField;
-        
+
         private System.Nullable<System.DateTime> CreatedAtField;
-        
+
         private System.Nullable<bool> DeletedField;
-        
+
         private string EventField;
-        
+
         private System.Guid IdField;
-        
+
         private string NameField;
-        
+
         private string PathField;
-        
+
         private MyPhotos.Persistence.Person[] PeopleField;
-        
+
         private string PlaceField;
-        
+
         private MyPhotos.Persistence.Property[] PropertiesField;
-        
+
         private System.Nullable<bool> VideoField;
-        
+
         public System.Runtime.Serialization.ExtensionDataObject ExtensionData
         {
             get
@@ -52,7 +52,7 @@ namespace MyPhotos.Persistence
                 this.extensionDataField = value;
             }
         }
-        
+
         [System.Runtime.Serialization.DataMemberAttribute()]
         public System.Nullable<System.DateTime> CreatedAt
         {
@@ -65,7 +65,7 @@ namespace MyPhotos.Persistence
                 this.CreatedAtField = value;
             }
         }
-        
+
         [System.Runtime.Serialization.DataMemberAttribute()]
         public System.Nullable<bool> Deleted
         {
@@ -78,7 +78,7 @@ namespace MyPhotos.Persistence
                 this.DeletedField = value;
             }
         }
-        
+
         [System.Runtime.Serialization.DataMemberAttribute()]
         public string Event
         {
@@ -91,7 +91,7 @@ namespace MyPhotos.Persistence
                 this.EventField = value;
             }
         }
-        
+
         [System.Runtime.Serialization.DataMemberAttribute()]
         public System.Guid Id
         {
@@ -104,7 +104,7 @@ namespace MyPhotos.Persistence
                 this.IdField = value;
             }
         }
-        
+
         [System.Runtime.Serialization.DataMemberAttribute()]
         public string Name
         {
@@ -117,7 +117,7 @@ namespace MyPhotos.Persistence
                 this.NameField = value;
             }
         }
-        
+
         [System.Runtime.Serialization.DataMemberAttribute()]
         public string Path
         {
@@ -130,7 +130,7 @@ namespace MyPhotos.Persistence
                 this.PathField = value;
             }
         }
-        
+
         [System.Runtime.Serialization.DataMemberAttribute()]
         public MyPhotos.Persistence.Person[] People
         {
@@ -143,7 +143,7 @@ namespace MyPhotos.Persistence
                 this.PeopleField = value;
             }
         }
-        
+
         [System.Runtime.Serialization.DataMemberAttribute()]
         public string Place
         {
@@ -156,7 +156,7 @@ namespace MyPhotos.Persistence
                 this.PlaceField = value;
             }
         }
-        
+
         [System.Runtime.Serialization.DataMemberAttribute()]
         public MyPhotos.Persistence.Property[] Properties
         {
@@ -169,7 +169,7 @@ namespace MyPhotos.Persistence
                 this.PropertiesField = value;
             }
         }
-        
+
         [System.Runtime.Serialization.DataMemberAttribute()]
         public System.Nullable<bool> Video
         {
@@ -183,19 +183,19 @@ namespace MyPhotos.Persistence
             }
         }
     }
-    
+
     [System.Diagnostics.DebuggerStepThroughAttribute()]
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Runtime.Serialization", "4.0.0.0")]
-    [System.Runtime.Serialization.DataContractAttribute(Name="Person", Namespace="http://schemas.datacontract.org/2004/07/MyPhotos.Persistence")]
+    [System.Runtime.Serialization.DataContractAttribute(Name = "Person", Namespace = "http://schemas.datacontract.org/2004/07/MyPhotos.Persistence")]
     public partial class Person : object, System.Runtime.Serialization.IExtensibleDataObject
     {
-        
+
         private System.Runtime.Serialization.ExtensionDataObject extensionDataField;
-        
+
         private System.Guid IdField;
-        
+
         private string NameField;
-        
+
         public System.Runtime.Serialization.ExtensionDataObject ExtensionData
         {
             get
@@ -207,7 +207,7 @@ namespace MyPhotos.Persistence
                 this.extensionDataField = value;
             }
         }
-        
+
         [System.Runtime.Serialization.DataMemberAttribute()]
         public System.Guid Id
         {
@@ -220,7 +220,7 @@ namespace MyPhotos.Persistence
                 this.IdField = value;
             }
         }
-        
+
         [System.Runtime.Serialization.DataMemberAttribute()]
         public string Name
         {
@@ -234,21 +234,21 @@ namespace MyPhotos.Persistence
             }
         }
     }
-    
+
     [System.Diagnostics.DebuggerStepThroughAttribute()]
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Runtime.Serialization", "4.0.0.0")]
-    [System.Runtime.Serialization.DataContractAttribute(Name="Property", Namespace="http://schemas.datacontract.org/2004/07/MyPhotos.Persistence")]
+    [System.Runtime.Serialization.DataContractAttribute(Name = "Property", Namespace = "http://schemas.datacontract.org/2004/07/MyPhotos.Persistence")]
     public partial class Property : object, System.Runtime.Serialization.IExtensibleDataObject
     {
-        
+
         private System.Runtime.Serialization.ExtensionDataObject extensionDataField;
-        
+
         private System.Guid IdField;
-        
+
         private string NameField;
-        
+
         private string ValueField;
-        
+
         public System.Runtime.Serialization.ExtensionDataObject ExtensionData
         {
             get
@@ -260,7 +260,7 @@ namespace MyPhotos.Persistence
                 this.extensionDataField = value;
             }
         }
-        
+
         [System.Runtime.Serialization.DataMemberAttribute()]
         public System.Guid Id
         {
@@ -273,7 +273,7 @@ namespace MyPhotos.Persistence
                 this.IdField = value;
             }
         }
-        
+
         [System.Runtime.Serialization.DataMemberAttribute()]
         public string Name
         {
@@ -286,7 +286,7 @@ namespace MyPhotos.Persistence
                 this.NameField = value;
             }
         }
-        
+
         [System.Runtime.Serialization.DataMemberAttribute()]
         public string Value
         {
@@ -304,38 +304,38 @@ namespace MyPhotos.Persistence
 
 
 [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
-[System.ServiceModel.ServiceContractAttribute(ConfigurationName="IMyPhotosWcf")]
+[System.ServiceModel.ServiceContractAttribute(ConfigurationName = "IMyPhotosWcf")]
 public interface IMyPhotosWcf
 {
-    
-    [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IMyPhotosWcf/CreatePhoto", ReplyAction="http://tempuri.org/IMyPhotosWcf/CreatePhotoResponse")]
+
+    [System.ServiceModel.OperationContractAttribute(Action = "http://tempuri.org/IMyPhotosWcf/CreatePhoto", ReplyAction = "http://tempuri.org/IMyPhotosWcf/CreatePhotoResponse")]
     System.Guid CreatePhoto(string path, string name, string @event, string place, string[] persons);
-    
-    [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IMyPhotosWcf/CreatePhoto", ReplyAction="http://tempuri.org/IMyPhotosWcf/CreatePhotoResponse")]
+
+    [System.ServiceModel.OperationContractAttribute(Action = "http://tempuri.org/IMyPhotosWcf/CreatePhoto", ReplyAction = "http://tempuri.org/IMyPhotosWcf/CreatePhotoResponse")]
     System.Threading.Tasks.Task<System.Guid> CreatePhotoAsync(string path, string name, string @event, string place, string[] persons);
-    
-    [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IMyPhotosWcf/GetAllPhotos", ReplyAction="http://tempuri.org/IMyPhotosWcf/GetAllPhotosResponse")]
+
+    [System.ServiceModel.OperationContractAttribute(Action = "http://tempuri.org/IMyPhotosWcf/GetAllPhotos", ReplyAction = "http://tempuri.org/IMyPhotosWcf/GetAllPhotosResponse")]
     MyPhotos.Persistence.Photo[] GetAllPhotos();
-    
-    [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IMyPhotosWcf/GetAllPhotos", ReplyAction="http://tempuri.org/IMyPhotosWcf/GetAllPhotosResponse")]
+
+    [System.ServiceModel.OperationContractAttribute(Action = "http://tempuri.org/IMyPhotosWcf/GetAllPhotos", ReplyAction = "http://tempuri.org/IMyPhotosWcf/GetAllPhotosResponse")]
     System.Threading.Tasks.Task<MyPhotos.Persistence.Photo[]> GetAllPhotosAsync();
-    
-    [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IMyPhotosWcf/AddCustomProperty", ReplyAction="http://tempuri.org/IMyPhotosWcf/AddCustomPropertyResponse")]
+
+    [System.ServiceModel.OperationContractAttribute(Action = "http://tempuri.org/IMyPhotosWcf/AddCustomProperty", ReplyAction = "http://tempuri.org/IMyPhotosWcf/AddCustomPropertyResponse")]
     void AddCustomProperty(string id, string name, string value);
-    
-    [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IMyPhotosWcf/AddCustomProperty", ReplyAction="http://tempuri.org/IMyPhotosWcf/AddCustomPropertyResponse")]
+
+    [System.ServiceModel.OperationContractAttribute(Action = "http://tempuri.org/IMyPhotosWcf/AddCustomProperty", ReplyAction = "http://tempuri.org/IMyPhotosWcf/AddCustomPropertyResponse")]
     System.Threading.Tasks.Task AddCustomPropertyAsync(string id, string name, string value);
-    
-    [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IMyPhotosWcf/GetFilteredPhotos", ReplyAction="http://tempuri.org/IMyPhotosWcf/GetFilteredPhotosResponse")]
+
+    [System.ServiceModel.OperationContractAttribute(Action = "http://tempuri.org/IMyPhotosWcf/GetFilteredPhotos", ReplyAction = "http://tempuri.org/IMyPhotosWcf/GetFilteredPhotosResponse")]
     MyPhotos.Persistence.Photo[] GetFilteredPhotos(string property, string value);
-    
-    [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IMyPhotosWcf/GetFilteredPhotos", ReplyAction="http://tempuri.org/IMyPhotosWcf/GetFilteredPhotosResponse")]
+
+    [System.ServiceModel.OperationContractAttribute(Action = "http://tempuri.org/IMyPhotosWcf/GetFilteredPhotos", ReplyAction = "http://tempuri.org/IMyPhotosWcf/GetFilteredPhotosResponse")]
     System.Threading.Tasks.Task<MyPhotos.Persistence.Photo[]> GetFilteredPhotosAsync(string property, string value);
-    
-    [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IMyPhotosWcf/GetAllPropertiesAsStrings", ReplyAction="http://tempuri.org/IMyPhotosWcf/GetAllPropertiesAsStringsResponse")]
+
+    [System.ServiceModel.OperationContractAttribute(Action = "http://tempuri.org/IMyPhotosWcf/GetAllPropertiesAsStrings", ReplyAction = "http://tempuri.org/IMyPhotosWcf/GetAllPropertiesAsStringsResponse")]
     string[] GetAllPropertiesAsStrings();
-    
-    [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IMyPhotosWcf/GetAllPropertiesAsStrings", ReplyAction="http://tempuri.org/IMyPhotosWcf/GetAllPropertiesAsStringsResponse")]
+
+    [System.ServiceModel.OperationContractAttribute(Action = "http://tempuri.org/IMyPhotosWcf/GetAllPropertiesAsStrings", ReplyAction = "http://tempuri.org/IMyPhotosWcf/GetAllPropertiesAsStringsResponse")]
     System.Threading.Tasks.Task<string[]> GetAllPropertiesAsStringsAsync();
 }
 
@@ -348,76 +348,76 @@ public interface IMyPhotosWcfChannel : IMyPhotosWcf, System.ServiceModel.IClient
 [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
 public partial class MyPhotosWcfClient : System.ServiceModel.ClientBase<IMyPhotosWcf>, IMyPhotosWcf
 {
-    
+
     public MyPhotosWcfClient()
     {
     }
-    
-    public MyPhotosWcfClient(string endpointConfigurationName) : 
+
+    public MyPhotosWcfClient(string endpointConfigurationName) :
             base(endpointConfigurationName)
     {
     }
-    
-    public MyPhotosWcfClient(string endpointConfigurationName, string remoteAddress) : 
+
+    public MyPhotosWcfClient(string endpointConfigurationName, string remoteAddress) :
             base(endpointConfigurationName, remoteAddress)
     {
     }
-    
-    public MyPhotosWcfClient(string endpointConfigurationName, System.ServiceModel.EndpointAddress remoteAddress) : 
+
+    public MyPhotosWcfClient(string endpointConfigurationName, System.ServiceModel.EndpointAddress remoteAddress) :
             base(endpointConfigurationName, remoteAddress)
     {
     }
-    
-    public MyPhotosWcfClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) : 
+
+    public MyPhotosWcfClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) :
             base(binding, remoteAddress)
     {
     }
-    
+
     public System.Guid CreatePhoto(string path, string name, string @event, string place, string[] persons)
     {
         return base.Channel.CreatePhoto(path, name, @event, place, persons);
     }
-    
+
     public System.Threading.Tasks.Task<System.Guid> CreatePhotoAsync(string path, string name, string @event, string place, string[] persons)
     {
         return base.Channel.CreatePhotoAsync(path, name, @event, place, persons);
     }
-    
+
     public MyPhotos.Persistence.Photo[] GetAllPhotos()
     {
         return base.Channel.GetAllPhotos();
     }
-    
+
     public System.Threading.Tasks.Task<MyPhotos.Persistence.Photo[]> GetAllPhotosAsync()
     {
         return base.Channel.GetAllPhotosAsync();
     }
-    
+
     public void AddCustomProperty(string id, string name, string value)
     {
         base.Channel.AddCustomProperty(id, name, value);
     }
-    
+
     public System.Threading.Tasks.Task AddCustomPropertyAsync(string id, string name, string value)
     {
         return base.Channel.AddCustomPropertyAsync(id, name, value);
     }
-    
+
     public MyPhotos.Persistence.Photo[] GetFilteredPhotos(string property, string value)
     {
         return base.Channel.GetFilteredPhotos(property, value);
     }
-    
+
     public System.Threading.Tasks.Task<MyPhotos.Persistence.Photo[]> GetFilteredPhotosAsync(string property, string value)
     {
         return base.Channel.GetFilteredPhotosAsync(property, value);
     }
-    
+
     public string[] GetAllPropertiesAsStrings()
     {
         return base.Channel.GetAllPropertiesAsStrings();
     }
-    
+
     public System.Threading.Tasks.Task<string[]> GetAllPropertiesAsStringsAsync()
     {
         return base.Channel.GetAllPropertiesAsStringsAsync();
