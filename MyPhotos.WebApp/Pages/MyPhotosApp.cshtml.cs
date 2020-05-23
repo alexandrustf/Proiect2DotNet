96d1778fb76d0889ca967af4048abf489d96f05e
alexandrustf <37770439+alexandrustf@users.noreply.github.com>
2020-05-23 11:54:06 +0300

    created Service reference and myphotospage

---
+++ MyPhotos.WebApp/Pages/MyPhotosApp.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyPhotos.WebApp.Pages
+{
+    public class MyPhotosAppModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
