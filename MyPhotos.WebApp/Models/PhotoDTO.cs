1b7decc423b308027051423056c049ab21022488
alexandrustf <37770439+alexandrustf@users.noreply.github.com>
2020-05-23 12:11:32 +0300

    created Index Page

---
+++ MyPhotos.WebApp/Models/PhotoDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace MyPhotos.WebApp.Models
+{
+    [DataContract(IsReference = true)]
+    public partial class PhotoDTO
+    {
+        public PhotoDTO(Guid id, string path, string name)
+        {
+            Id = id;
+            Path = path;
+            Name = name;
+        }
+        [DataMember]
+        public System.Guid Id { get; set; }
+        [DataMember]
+        public string Path { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+    }
+}
---
+++ MyPhotos.WebApp/Pages/Photos/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceReference1;
+
+namespace MyPhotos.WebApp.Pages.Photos
+{
+    public class IndexModel : PageModel
+    {
+        MyPhotosWcfClient myPhotosWcfClient = new MyPhotosWcfClient();
+        public List<PhotoDTO> Photos { get; set; }
+
+        public IndexModel()
+        {
+            Photos = new List<PhotoDTO>();
+        }
+
+        public async Task OnGetAsync()
+        {
+            var posts = await myPhotosWcfClient.GetAllPhotosAsync();
+            foreach (var item in posts)
+            {
+                PhotoDTO pd = new PhotoDTO();
+                pd.Id = item.Id;
+                pd.Name = item.Name;
+                pd.Path = item.Path;
+
+                Photos.Add(pd);
+            }
+        }
+    }
+}
